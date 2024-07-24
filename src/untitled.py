57cb8bdac2268e9b09b989f02d03acdbf6d0a7cf
GrassBlock2016 <598926542@qq.com>
2024-07-24 16:18:16 +0800

    first commit

---
+++ src/untitled.py
@@ -0,0 +1,147 @@
+# -*- encoding=utf8 -*-
+__author__ = "GrassBlock2016"
+
+from airtest.core.api import *
+
+auto_setup(__file__)
+
+# 签到
+touch(Template(r"tpl1721805753649.png", record_pos=(-0.083, -0.107), resolution=(755, 461)))
+
+# 咖啡厅
+touch(Template(r"tpl1721805789773.png", record_pos=(-0.426, 0.258), resolution=(755, 461)))
+touch(Template(r"tpl1721805807035.png", record_pos=(-0.001, 0.104), resolution=(755, 461)))
+touch(Template(r"tpl1721805820557.png", record_pos=(0.394, 0.235), resolution=(755, 461)))
+touch(Template(r"tpl1721805827765.png", record_pos=(-0.002, 0.149), resolution=(755, 461)))
+touch(Template(r"tpl1721805836757.png", record_pos=(0.003, 0.242), resolution=(755, 461)))
+touch(Template(r"tpl1721805844416.png", record_pos=(0.203, -0.134), resolution=(755, 461)))
+
+# 日程
+touch(Template(r"tpl1721805880577.png", record_pos=(-0.333, 0.277), resolution=(755, 461)))
+touch(Template(r"tpl1721805914484.png", record_pos=(0.123, -0.128), resolution=(755, 461)))
+touch(Template(r"tpl1721805937073.png", record_pos=(0.411, 0.26), resolution=(755, 461)))
+touch(Template(r"tpl1721805946869.png", record_pos=(0.005, 0.132), resolution=(755, 461)))
+touch(Template(r"tpl1721805952594.png", record_pos=(0.001, 0.172), resolution=(755, 461)))
+# touch(Template(r"tpl1721806179079.png", record_pos=(-0.009, 0.228), resolution=(755, 461)))
+touch(Template(r"tpl1721806196875.png", record_pos=(0.006, 0.178), resolution=(755, 461)))
+touch(Template(r"tpl1721805963582.png", record_pos=(-0.239, 0.133), resolution=(755, 461)))
+touch(Template(r"tpl1721805952594.png", record_pos=(0.001, 0.172), resolution=(755, 461)))
+# touch(Template(r"tpl1721806179079.png", record_pos=(-0.009, 0.228), resolution=(755, 461)))
+touch(Template(r"tpl1721806196875.png", record_pos=(0.006, 0.178), resolution=(755, 461)))
+touch(Template(r"tpl1721805985906.png", record_pos=(0.385, -0.164), resolution=(755, 461)))
+touch(Template(r"tpl1721805999476.png", record_pos=(-0.455, -0.228), resolution=(755, 461)))
+touch(Template(r"tpl1721806023296.png", record_pos=(0.14, 0.115), resolution=(755, 461)))
+touch(Template(r"tpl1721805937073.png", record_pos=(0.411, 0.26), resolution=(755, 461)))
+touch(Template(r"tpl1721806048317.png", record_pos=(-0.005, 0.164), resolution=(755, 461)))
+touch(Template(r"tpl1721805952594.png", record_pos=(0.001, 0.172), resolution=(755, 461)))
+# touch(Template(r"tpl1721806179079.png", record_pos=(-0.009, 0.228), resolution=(755, 461)))
+touch(Template(r"tpl1721806196875.png", record_pos=(0.006, 0.178), resolution=(755, 461)))
+touch(Template(r"tpl1721806058939.png", record_pos=(-0.267, 0.164), resolution=(755, 461)))
+touch(Template(r"tpl1721805952594.png", record_pos=(0.001, 0.172), resolution=(755, 461)))
+# touch(Template(r"tpl1721806179079.png", record_pos=(-0.009, 0.228), resolution=(755, 461)))
+touch(Template(r"tpl1721806196875.png", record_pos=(0.006, 0.178), resolution=(755, 461)))
+touch(Template(r"tpl1721805985906.png", record_pos=(0.385, -0.164), resolution=(755, 461)))
+touch(Template(r"tpl1721805999476.png", record_pos=(-0.455, -0.228), resolution=(755, 461)))
+
+touch(Template(r"tpl1721806097002.png", record_pos=(0.125, 0.197), resolution=(755, 461)))
+touch(Template(r"tpl1721805937073.png", record_pos=(0.411, 0.26), resolution=(755, 461)))
+touch(Template(r"tpl1721806118066.png", record_pos=(-0.023, 0.164), resolution=(755, 461)))
+touch(Template(r"tpl1721805952594.png", record_pos=(0.001, 0.172), resolution=(755, 461)))
+# touch(Template(r"tpl1721806179079.png", record_pos=(-0.009, 0.228), resolution=(755, 461)))
+touch(Template(r"tpl1721806196875.png", record_pos=(0.006, 0.178), resolution=(755, 461)))
+touch(Template(r"tpl1721806137115.png", record_pos=(-0.3, 0.165), resolution=(755, 461)))
+
+touch(Template(r"tpl1721805952594.png", record_pos=(0.001, 0.172), resolution=(755, 461)))
+# touch(Template(r"tpl1721806179079.png", record_pos=(-0.009, 0.228), resolution=(755, 461)))
+touch(Template(r"tpl1721806196875.png", record_pos=(0.006, 0.178), resolution=(755, 461)))
+touch(Template(r"tpl1721805985906.png", record_pos=(0.385, -0.164), resolution=(755, 461)))
+touch(Template(r"tpl1721805999476.png", record_pos=(-0.455, -0.228), resolution=(755, 461)))
+touch(Template(r"tpl1721806303882.png", record_pos=(-0.455, -0.23), resolution=(755, 461)))
+
+# 小组签到
+touch(Template(r"tpl1721806358149.png", record_pos=(-0.059, 0.279), resolution=(755, 461)))
+touch(Template(r"tpl1721806371263.png", record_pos=(-0.456, -0.232), resolution=(755, 461)))
+
+# 领取邮件
+touch(Template(r"tpl1721806386557.png", record_pos=(0.393, -0.227), resolution=(755, 461)))
+touch(Template(r"tpl1721806422465.png", record_pos=(0.387, 0.266), resolution=(755, 461)))
+touch(Template(r"tpl1721806371263.png", record_pos=(-0.456, -0.232), resolution=(755, 461)))
+
+# 领取任务体力
+touch(Template(r"tpl1721806559098.png", record_pos=(-0.45, -0.075), resolution=(755, 461)))
+touch(Template(r"tpl1721806565536.png", record_pos=(0.394, 0.264), resolution=(755, 461)))
+touch(Template(r"tpl1721806577655.png", record_pos=(0.002, 0.236), resolution=(755, 461)))
+touch(Template(r"tpl1721806371263.png", record_pos=(-0.456, -0.232), resolution=(755, 461)))
+
+# 悬赏通缉 & 学院交流会 & 战术对抗赛
+touch(Template(r"tpl1721806609846.png", record_pos=(0.432, 0.173), resolution=(755, 461)))
+touch(Template(r"tpl1721806666832.png", record_pos=(0.06, 0.076), resolution=(755, 461)))
+touch(Template(r"tpl1721806674561.png", record_pos=(0.386, -0.071), resolution=(755, 461)))
+
+touch(Template(r"tpl1721806790865.png", record_pos=(0.372, 0.244), resolution=(755, 461)))
+touch(Template(r"tpl1721806808224.png", record_pos=(0.309, -0.025), resolution=(755, 461)))
+touch(Template(r"tpl1721806816840.png", record_pos=(0.23, 0.056), resolution=(755, 461)))
+touch(Template(r"tpl1721806840022.png", record_pos=(-0.002, 0.199), resolution=(755, 461)))
+sleep(5.0)
+touch(Template(r"tpl1721806825963.png", record_pos=(0.1, 0.132), resolution=(755, 461)))
+
+touch(Template(r"tpl1721806864003.png", record_pos=(0.387, 0.026), resolution=(755, 461)))
+touch(Template(r"tpl1721806790865.png", record_pos=(0.372, 0.244), resolution=(755, 461)))
+touch(Template(r"tpl1721806808224.png", record_pos=(0.309, -0.025), resolution=(755, 461)))
+touch(Template(r"tpl1721806816840.png", record_pos=(0.23, 0.056), resolution=(755, 461)))
+touch(Template(r"tpl1721806840022.png", record_pos=(-0.002, 0.199), resolution=(755, 461)))
+sleep(5.0)
+touch(Template(r"tpl1721806825963.png", record_pos=(0.1, 0.132), resolution=(755, 461)))
+
+touch(Template(r"tpl1721806953883.png", record_pos=(0.419, 0.12), resolution=(755, 461)))
+touch(Template(r"tpl1721806790865.png", record_pos=(0.372, 0.244), resolution=(755, 461)))
+touch(Template(r"tpl1721806808224.png", record_pos=(0.309, -0.025), resolution=(755, 461)))
+touch(Template(r"tpl1721806816840.png", record_pos=(0.23, 0.056), resolution=(755, 461)))
+touch(Template(r"tpl1721806840022.png", record_pos=(-0.002, 0.199), resolution=(755, 461)))
+sleep(5.0)
+touch(Template(r"tpl1721806825963.png", record_pos=(0.1, 0.132), resolution=(755, 461)))
+    
+touch(Template(r"tpl1721807011545.png", record_pos=(-0.456, -0.228), resolution=(755, 461)))
+
+touch(Template(r"tpl1721807121112.png", record_pos=(0.048, 0.202), resolution=(755, 461)))
+touch(Template(r"tpl1721807129916.png", record_pos=(0.397, -0.064), resolution=(755, 461)))
+touch(Template(r"tpl1721806790865.png", record_pos=(0.372, 0.244), resolution=(755, 461)))
+touch(Template(r"tpl1721806808224.png", record_pos=(0.309, -0.025), resolution=(755, 461)))
+touch(Template(r"tpl1721806816840.png", record_pos=(0.23, 0.056), resolution=(755, 461)))
+touch(Template(r"tpl1721806840022.png", record_pos=(-0.002, 0.199), resolution=(755, 461)))
+sleep(5.0)
+touch(Template(r"tpl1721806825963.png", record_pos=(0.1, 0.132), resolution=(755, 461)))
+
+touch(Template(r"tpl1721807191066.png", record_pos=(0.398, 0.047), resolution=(755, 461)))
+touch(Template(r"tpl1721806790865.png", record_pos=(0.372, 0.244), resolution=(755, 461)))
+touch(Template(r"tpl1721806808224.png", record_pos=(0.309, -0.025), resolution=(755, 461)))
+touch(Template(r"tpl1721806816840.png", record_pos=(0.23, 0.056), resolution=(755, 461)))
+touch(Template(r"tpl1721806840022.png", record_pos=(-0.002, 0.199), resolution=(755, 461)))
+sleep(5.0)
+touch(Template(r"tpl1721806825963.png", record_pos=(0.1, 0.132), resolution=(755, 461)))
+
+touch(Template(r"tpl1721807419305.png", record_pos=(0.397, 0.157), resolution=(755, 461)))
+touch(Template(r"tpl1721806790865.png", record_pos=(0.372, 0.244), resolution=(755, 461)))
+touch(Template(r"tpl1721806808224.png", record_pos=(0.309, -0.025), resolution=(755, 461)))
+touch(Template(r"tpl1721806816840.png", record_pos=(0.23, 0.056), resolution=(755, 461)))
+touch(Template(r"tpl1721806840022.png", record_pos=(-0.002, 0.199), resolution=(755, 461)))
+sleep(5.0)
+touch(Template(r"tpl1721806825963.png", record_pos=(0.1, 0.132), resolution=(755, 461)))    
+touch(Template(r"tpl1721807011545.png", record_pos=(-0.456, -0.228), resolution=(755, 461)))
+touch(Template(r"tpl1721807707439.png", record_pos=(0.369, 0.074), resolution=(755, 461)))
+touch(Template(r"tpl1721807718057.png", record_pos=(-0.223, 0.046), resolution=(755, 461)))
+touch(Template(r"tpl1721807764186.png", record_pos=(0.002, 0.236), resolution=(755, 461)))
+touch(Template(r"tpl1721807724890.png", record_pos=(-0.223, 0.107), resolution=(755, 461)))
+touch(Template(r"tpl1721807764186.png", record_pos=(0.002, 0.236), resolution=(755, 461)))
+touch(Template(r"tpl1721807799555.png", record_pos=(-0.112, -0.03), resolution=(755, 461)))
+touch(Template(r"tpl1721807808260.png", record_pos=(0.005, 0.187), resolution=(755, 461)))
+touch(Template(r"tpl1721807818504.png", record_pos=(0.407, 0.264), resolution=(755, 461)))
+touch(Template(r"tpl1721807826137.png", record_pos=(-0.001, 0.158), resolution=(755, 461)))
+touch(Template(r"tpl1721807844792.png", record_pos=(-0.455, -0.232), resolution=(755, 461)))
+
+# 领取任务奖励
+touch(Template(r"tpl1721807844792.png", record_pos=(-0.455, -0.232), resolution=(755, 461)))
+touch(Template(r"tpl1721806559098.png", record_pos=(-0.45, -0.075), resolution=(755, 461)))
+touch(Template(r"tpl1721806565536.png", record_pos=(0.394, 0.264), resolution=(755, 461)))
+touch(Template(r"tpl1721806577655.png", record_pos=(0.002, 0.236), resolution=(755, 461)))
+touch(Template(r"tpl1721806371263.png", record_pos=(-0.456, -0.232), resolution=(755, 461)))
